1f51a7fac77ff918cf23c25bff596a58c46493f7
Thanh Pham <thanhphamlike@gmail.com>
2026-06-12 00:49:33 -0400

    setting automated workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/portfolioLandingPage",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
